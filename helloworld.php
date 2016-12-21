dd4b7eef9bdd5fccef1ccac9b6883f945eebb007
sunwebgithub <github@sunwebtechnologies.com>
2016-12-21 18:36:43 +0530

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,4 +1,4 @@
 <?php
-$connect = mysql_connect("phpmysql-153207:asia-east1:phpmysql1","root","") or die ("Error: could not connect to database");
-echo $connect;
+//$connect = mysql_connect("phpmysql-153207:asia-east1:phpmysql1","root","") or die ("Error: could not connect to database");
+//echo $connect;
 echo 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, ';
