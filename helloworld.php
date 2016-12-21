b534a8c824fd834e1023bde0e68ea989d14d9f4a
sunwebgithub <github@sunwebtechnologies.com>
2016-12-21 22:34:04 +0530

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -8,7 +8,7 @@
     $port = 3306;                               //The port #. It is always 3306
 
 
-$con = mysqli_connect("localhost","test","123","guestbook", null, "/cloudsql/phpmysql1");
+$con = mysqli_connect("localhost","test","123","guestbook");
 
 // Check connection
 if (mysqli_connect_errno())
