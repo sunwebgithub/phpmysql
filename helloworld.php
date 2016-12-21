d466d49ec8f402943b5c12a7ebd34c8cfe052d83
sunwebgithub <github@sunwebtechnologies.com>
2016-12-21 22:19:27 +0530

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,6 +1,5 @@
 <?php
-//$connect = mysql_connect("localhost","test","123") or die ("Error: could not connect to database");
-//echo $connect;
+
 
     $host = "localhost";   //See Step 3 about how to get host name
     $user = "test";                     //Your Cloud 9 username
@@ -8,15 +7,11 @@
     $db = "guestbook";                          //Your database name you want to connect to
     $port = 3306;                               //The port #. It is always 3306
 
-    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
-echo $connection;
 
-//And now to perform a simple query to make sure it's working
-    $query = "SELECT * FROM entries";
-    $result = mysqli_query($connection, $query);
+$con = mysqli_connect("localhost","test","123","guestbook");
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "The ID is: " . $row['entryID '] . " and the guestname is: " . $row['guestName'];
-    }
-
-//echo 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, ';
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
