b049581ec0221aefdf829aa4766ecb2f072dc678
sunwebgithub <github@sunwebtechnologies.com>
2016-12-21 23:20:39 +0530

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -15,6 +15,18 @@ if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }else{
-    mysqli_query($con,"SELECT * FROM entries");
-    echo "Affected rows: " . mysqli_affected_rows($con);
+    $sql="SELECT * FROM entries";
+
+if ($result=mysqli_query($con,$sql))
+  {
+  // Fetch one and one row
+  while ($row=mysqli_fetch_row($result))
+    {
+    printf ("%s (%s)\n",$row[0],$row[1]);
+    }
+  // Free result set
+  mysqli_free_result($result);
+}
+
+mysqli_close($con);
 }
